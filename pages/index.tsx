28c60ff82860dc303f7f63063bebefc161016ebc
gabriel <gabriel.r.lacerda@hotmail.com>
2023-05-02 21:15:59 -0300

    criando layouts de docs components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import Button from '../src/components/Button';
+
 export default function HomeScreen() {
   
   return (
     <div>
       <h1>Home Page!</h1>
+      <Button>
+        Botão simples!
+      </Button>
     </div>
   )
 }
